1fe0d749fc680330de10cde2275e10ca0c4a6acd
Robbie Lamb <robbie.lamb@webfilings.com>
2014-01-19 16:52:42 -0700

    Updating the scrollbar so the user can drag it around.

--- examples/random_people/random_people.js
+++ examples/random_people/random_people.js
@@ -5,8 +5,8 @@
     var personTemplate = $('#person-template').html();
     Mustache.parse(personTemplate);
     var cache = {};
-    var TOTAL_ITEMS = 15,
-        MIN_NUMBER_OF_VIRTUAL_ITEMS = 4;
+    var TOTAL_ITEMS = 1000,
+        MIN_NUMBER_OF_VIRTUAL_ITEMS = 50;
 
 
 
@@ -86,10 +86,6 @@
     scrollList.onPlaceholderRendered(function(sender, args) {
         var container = args.placeholder.contentContainer;
         $(container).addClass('loading');
-        // var el = document.createElement('img');
-        // el.className = 'loading';
-
-        // container.appendChild(el);
     });
 
     scrollList.render();
@@ -104,23 +100,80 @@
 
 
     // SCROLL BAR
-    var scrollBarEL = document.getElementById('scroll-bar');
+    var scrollbarEL = document.getElementById('scroll-bar');
     var layout = scrollList.getLayout();
     var virtualHeight = layout.getSize().height,          // virtual height of the content
         viewportHeight = layout.getViewportSize().height; // This could change on window resize
 
     // Calculate the size of the scrollbar depending on the virtual height
-    var scrollBarHeight = Math.max(16, ((viewportHeight/virtualHeight) * viewportHeight))
-    scrollBarEL.style.height = scrollBarHeight + 'px';
+    var scrollbarHeight = Math.max(16, ((viewportHeight/virtualHeight) * viewportHeight))
+    scrollbarEL.style.height = scrollbarHeight + 'px';
 
-    scrollList.onCurrentItemChanged(function(/*sender, args*/) {
+    scrollList.getListMap().onTranslationChanged(function(/*sender, args*/) {
+        if (scrollbarScrolling) {
+          return;
+        }
         setTimeout(function() {
             var currentPosition = layout.getVisiblePosition().top;
-            var availableScrollbarHeight = viewportHeight - scrollBarHeight;
+            var availableScrollbarHeight = viewportHeight - scrollbarHeight;
             var scrollableVirtualHeight = virtualHeight - viewportHeight;
             var translatedPosition = availableScrollbarHeight / scrollableVirtualHeight * currentPosition;
-            scrollBarEL.style.top = translatedPosition + 'px';
+            scrollbarEL.style.top = translatedPosition + 'px';
         }, 0);
+    });
+
+    // SCROLL BAR INTERACTION
+    var clickOffset;
+    var scrollbarScrolling = false;
+
+    function updateScrollBar(event) {
+        // Don't go past the window bounds
+        var scrollbarPos = Math.max(0, event.y - clickOffset);
+        scrollbarPos = Math.min(scrollbarPos, viewportHeight - scrollbarHeight);
+
+        scrollbarEL.style.top = scrollbarPos + 'px';
+
+        // Use the ratio of scrollbar position inside the scrolling area to calculate
+        // the current item we should be interested in.
+        var positionOfInterest = (scrollbarPos / (viewportHeight - scrollbarHeight)) * (TOTAL_ITEMS - 2) + 1;
+
+        // The whole number part is the index of the item we care about
+        // The part after the decimal place will be used to calculate
+        // a 'y' offset based on the height of the item.
+        var indexOfItem = Math.floor(positionOfInterest);
+        var remainder = positionOfInterest - indexOfItem;
+
+
+        // Lazily figure out the height of the element.
+        // Everything is 310px high. 300 for the actual element, and 10 for gap
+        // Except the the first and last items, they also need to include the padding.
+        var objHeight = 310;
+        if (indexOfItem === 0 || indexOfItem === (TOTAL_ITEMS)){
+          objHeight = 315;
+        }
+
+        scrollList.scrollTo({
+          index: indexOfItem,
+          center: {x: 0, y: objHeight * remainder}
+        });
+    };
+
+    function stopUpdatingScrollbar(event) {
+        clickOffset = undefined;
+        scrollbarScrolling = false;
+        document.removeEventListener('mousemove', updateScrollBar);
+        removeDocumentEventWatching();
+    };
+
+    function removeDocumentEventWatching() {
+      document.removeEventListener('mouseup', stopUpdatingScrollbar);
+    };
+
+    scrollbarEL.addEventListener('mousedown', function(event) {
+        clickOffset = event.y - scrollbarEL.offsetTop;
+        scrollbarScrolling = true;
+        document.addEventListener('mousemove', updateScrollBar);
+        document.addEventListener('mouseup', stopUpdatingScrollbar);
     });
 
 })();
