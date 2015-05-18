40b67aa145074343bea233079e2c185293e5bae4
Tod Bachman <tod.bachman@workiva.com>
2015-05-18 11:57:29 -0600

    Port EventTypes to Dart as enum

--- lib/awesome_map/EventTypes.dart
+++ lib/awesome_map/EventTypes.dart
@@ -14,133 +14,58 @@
  * limitations under the License.
  */
 
-define(function() {
-    'use strict';
+enum EventTypes {
+  /// User right clicks on desktop.
+  contextMenu,
 
-    /**
-     * @classdesc
-     *
-     * EventTypes represent the type of events encapsulted by an {@link IneractionEvent}.
-     *
-     * @exports EventTypes
-     */
-    var EventTypes = {
+  /// User double taps on touch or double clicks on desktop.
+  doubleTap,
 
-        /**
-         * User right clicks on desktop.
-         * @type {string}
-         */
-        CONTEXT_MENU: 'contextmenu',
+  /// User drags on touch or clicks and drags on desktop.
+  drag,
 
-        /**
-         * User double taps on touch or double clicks on desktop.
-         * @type {string}
-         */
-        DOUBLE_TAP: 'doubletap',
+  /// User stops dragging due to a release or a change in gesture.
+  dragEnd,
 
-        /**
-         * User drags on touch or clicks and drags on desktop.
-         * @type {string}
-         */
-        DRAG: 'drag',
+  /// User starts dragging.
+  dragStart,
 
-        /**
-         * User stops dragging due to a release or a change in gesture.
-         * @type {string}
-         */
-        DRAG_END: 'dragend',
+  /// User taps and holds on touch or clicks and holds on desktop.
+  hold,
 
-        /**
-         * User starts dragging.
-         * @type {string}
-         */
-        DRAG_START: 'dragstart',
+  /// User generates a mousemove event on desktop.
+  mouseMove,
 
-        /**
-         * User taps and holds on touch or clicks and holds on desktop.
-         * @type {string}
-         */
-        HOLD: 'hold',
+  /// User generates a mousewheel event on desktop.
+  mouseWheel,
 
-        /**
-         * User generates a mousemove event on desktop.
-         * @type {string}
-         */
-        MOUSE_MOVE: 'mousemove',
+  /// User began spinning the mouse wheel.
+  mouseWheelStart,
 
-        /**
-         * User generates a mousewheel event on desktop.
-         * @type {string}
-         */
-        MOUSE_WHEEL: 'mousewheel',
+  /// User has finished spinning the mouse wheel.
+  mouseWheelEnd,
 
-        /**
-         * User began spinning the mouse wheel.
-         * @type {string}
-         */
-        MOUSE_WHEEL_START: 'mousewheelstart',
+  /// User releases all fingers on touch or releases mouse-clicks on desktop.
+  release,
 
-        /**
-         * User has finished spinning the mouse wheel.
-         * @type {string}
-         */
-        MOUSE_WHEEL_END: 'mousewheelend',
+  /// The viewport of the EventSynthesizer's host is resized.
+  resize,
 
-        /**
-         * User is holding on a touchscreen and a box shows up to indicate release will show a
-         * context menu.
-         * @type {String}
-         */
-        MS_HOLD_VISUAL: 'MSHoldVisual',
+  /// User swipes on touch or clicks, drags and releases quickly on desktop.
+  swipe,
 
-        /**
-         * User releases all fingers on touch or releases mouse-clicks on desktop.
-         * @type {string}
-         */
-        RELEASE: 'release',
+  /// User taps and releases on touch or clicks and releases on desktop.
+  tap,
 
-        /**
-         * The viewport of the EventSynthesizer's host is resized.
-         * @type {string}
-         */
-        RESIZE: 'resize',
+  /// User touches on touch or hovers the mouse on desktop.
+  touch,
 
-        /**
-         * User swipes on touch or clicks, drags and releases quickly on desktop.
-         * @type {string}
-         */
-        SWIPE: 'swipe',
+  /// User pinches on touch.
+  transform,
 
-        /**
-         * User taps and releases on touch or clicks and releases on desktop.
-         * @type {string}
-         */
-        TAP: 'tap',
+  /// User stops pinching on touch.
+  transformEnd,
 
-        /**
-         * User touches on touch or hovers the mouse on desktop.
-         * @type {string}
-         */
-        TOUCH: 'touch',
-
-        /**
-         * User pinches on touch.
-         * @type {string}
-         */
-        TRANSFORM: 'transform',
-
-        /**
-         * User stops pinching on touch.
-         * @type {string}
-         */
-        TRANSFORM_END: 'transformend',
-
-        /**
-         * User starts pinching.
-         * @type {string}
-         */
-        TRANSFORM_START: 'transformstart'
-    };
-
-    return EventTypes;
-});
+  /// User starts pinching.
+  transformStart
+}
